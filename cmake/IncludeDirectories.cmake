eb3b860b1afac0bb9a8105feb44190db05aeb7df
octopus <912554887@qq.com>
2023-08-13 00:17:09 +0800

    rename my_math to libmath

--- cmake/IncludeDirectories.cmake
+++ cmake/IncludeDirectories.cmake
@@ -1,3 +1,3 @@
 # 添加include目录
 include_directories(${CMAKE_CURRENT_SOURCE_DIR})
-include_directories(${CMAKE_CURRENT_SOURCE_DIR}/modules/my_math/src)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/modules/libmath/src)
