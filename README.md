53966024a9efbbb8fe5cb383c538fe783c1b86a6
octopus <912554887@qq.com>
2023-08-12 17:04:42 +0800

    重构, 添加modules目录

--- README.md
+++ README.md
@@ -4,13 +4,16 @@ $ cmake ../ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DTESTS=ON
 $ make
 ```
 
+# 学习
+1. cmake文档
+2. vcpkg文档
+3. gtest，学会如何在c++中构造ut
+4. beachmark，学会如何在c++中进行基准测试
+5. c++20 规范
+
 # TODO
 1. 集成`vcpkg`
-2. 集成`Boost`、`JSON`、`Protocol Buffer`等
+2. 集成常用的c++库, `Boost`、`JSON`、`Protocol Buffer`、`gtest`、`beachmark`等
 3. 优化使用姿势
+4. 添加ci, 支持多平台
 
-# 学习
-1. gtest
-2. beachmark
-3. asio
-4. ...
--- main/main.cpp
+++ main/main.cpp
@@ -13,6 +13,45 @@ void printVersion()
     std::cout << "Git Commit         : " << PROJECT_COMMIT_HASH << std::endl;
 }
 
+/*
+#include <fmt/format.h>
+#include <cxxopts.hpp>
+
+#include <range/v3/view.hpp>
+
+namespace view = ranges::views;
+
+int fib(int x)
+{
+    int a = 0, b = 1;
+
+    for (int it : view::repeat(0) | view::take(x))
+    {
+        (void)it;
+        int tmp = a;
+        a += b;
+        b = tmp;
+    }
+
+    return a;
+}
+
+int main0(int argc, char** argv)
+{
+    cxxopts::Options options("fibo", "Print the fibonacci sequence up to a value 'n'");
+    options.add_options()(
+        "n,value", "The value to print to", cxxopts::value<int>()->default_value("10"));
+
+    auto result = options.parse(argc, argv);
+    auto n = result["value"].as<int>();
+
+    for (int x : view::iota(1) | view::take(n))
+    {
+        fmt::print("fib({}) = {}\n", x, fib(x));
+    }
+}
+*/
+
 int main(int argc, char* argv[])
 {
     if (argc >= 1 && ((0 == strncmp(argv[1], "-v", 2) || 0 == strncmp(argv[1], "--version", 8))))
