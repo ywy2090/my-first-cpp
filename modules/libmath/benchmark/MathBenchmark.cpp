b661e9bd66c35f09f7aecd3c75f194339c659343
octopus <912554887@qq.com>
2023-09-06 23:04:40 +0800

    test

--- modules/libmath/benchmark/MathBenchmark.cpp
+++ modules/libmath/benchmark/MathBenchmark.cpp
@@ -2,14 +2,24 @@
 
 
 // https://github.com/google/benchmark/blob/main/docs/user_guide.md
+static void DoSetup(const benchmark::State& state) 
+{
+
+}
+
+static void DoTeardown(const benchmark::State& state) 
+{
+}
+
 
 static void BM_StringCreation(benchmark::State& state)
 {
     for (auto _ : state)
         std::string empty_string;
 }
+
 // Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+BENCHMARK(BM_StringCreation)->Arg(1)->Arg(3)->Threads(16)->Threads(32)->Setup(DoSetup)->Teardown(DoTeardown);
 
 // Define another benchmark
 static void BM_StringCopy(benchmark::State& state)
@@ -18,6 +28,8 @@ static void BM_StringCopy(benchmark::State& state)
     for (auto _ : state)
         std::string copy(x);
 }
-BENCHMARK(BM_StringCopy);
+BENCHMARK(BM_StringCopy)->Arg(1)->Arg(3)->Threads(16)->Threads(32)->Setup(DoSetup)->Teardown(DoTeardown);
+
+
 
 BENCHMARK_MAIN();
