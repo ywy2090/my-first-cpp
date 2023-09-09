f89650b9c844b13186c81c48e9667f993249c7dc
octopus <912554887@qq.com>
2023-09-09 13:11:53 +0800

    modify spdlog format

--- modules/liblogger/src/Logger.cpp
+++ modules/liblogger/src/Logger.cpp
@@ -1,6 +1,7 @@
 #include "Logger.h"
 #include "spdlog/async.h"
 #include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/daily_file_sink.h"
 #include "spdlog/sinks/rotating_file_sink.h"
 #include <spdlog/common.h>
 #include <spdlog/spdlog.h>
@@ -16,10 +17,14 @@ void Logger::startLog(const LogConf& logConf)
 
     m_running = true;
 
-    std::string logPattern = "[%Y %H:%M:%S %f] [thread %t] [%l] [%s %# %!]";
+    std::string logPattern = "[%Y %H:%M:%S %f] [%l] [thread %t] [%s:%#] [%!]";
     auto rotateLogger = spdlog::rotating_logger_mt<spdlog::async_factory>("rotate_logger",
         logConf.logDir + "/" + logConf.logName, logConf.logFileSize, logConf.logFileCount);
 
+    // auto dailyLogger = spdlog::daily_logger_mt<spdlog::async_factory>(
+    //     "daily_logger", logConf.logDir + "/" + logConf.logName, 0, 0, logConf.logFileCount);
+
+    rotateLogger->set_pattern(logPattern);
     rotateLogger->flush_on(spdlog::level::warn);
     rotateLogger->set_level(spdlog::level::from_str(logConf.logLevel));
     spdlog::set_default_logger(rotateLogger);
